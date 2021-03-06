7236bce527341770841197029c07a3c56784894d
Serhiy Brytskyy <sbrytskyy@gmail.com>
2021-03-05 16:16:12 -0800

    Unit Test using Mockito. Cosmetic.

--- src/test/java/guru/springframework/services/RecipeServiceImplTest.java
+++ src/test/java/guru/springframework/services/RecipeServiceImplTest.java
@@ -9,28 +9,30 @@ import org.mockito.MockitoAnnotations;
 
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class RecipeServiceImplTest {
 
+    private RecipeService recipeService;
+
     @Mock
     private RecipeRepository recipeRepository;
-
-    private RecipeService recipeService;
 
     @Before
     public void setUp() {
         MockitoAnnotations.openMocks(this);
 
-        when(recipeRepository.findAll()).thenReturn(Set.of(new Recipe()));
 
         recipeService = new RecipeServiceImpl(recipeRepository);
     }
 
     @Test
-    public void getAllRecipes() {
+    public void getRecipes() {
+        when(recipeRepository.findAll()).thenReturn(Set.of(new Recipe()));
+
         Set<Recipe> recipes = recipeService.getRecipes();
+
         assertEquals(recipes.size(), 1);
         verify(recipeRepository, times(1)).findAll();
     }
