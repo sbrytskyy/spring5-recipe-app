a0dfc4281267a01b0757dd62d53c2d1ee1f92d0e
Serhiy Brytskyy <sbrytskyy@gmail.com>
2021-03-05 16:42:16 -0800

    Added IndexControllerTest. Cosmetic

--- src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -1,15 +1,11 @@
 package guru.springframework.controllers;
 
-import guru.springframework.domain.Recipe;
 import guru.springframework.services.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.AdditionalMatchers;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
-
-import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
@@ -31,17 +27,11 @@ public class IndexControllerTest {
 
     @Test
     public void getIndexPage() {
-//        Set<Recipe> recipes = Set.of(new Recipe());
-//        when(recipeService.getRecipes()).thenReturn(recipes);
-//        when(model.getAttribute("recipes")).thenReturn(recipes);
-
         String viewName = indexController.getIndexPage(model);
 
         assertEquals(viewName, "index");
-        assertEquals(model.getAttribute("recipes"), recipes);
 
         verify(recipeService, times(1)).getRecipes();
         verify(model, times(1)).addAttribute(eq("recipes"), anySet());
-        verify(model, times(0)).addAttribute(AdditionalMatchers.not(eq("recipes")), anyIterable());
     }
 }
